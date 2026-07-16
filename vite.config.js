0e864484194e9cc8aec5e234000c2dd2cee477d9
RickyMandich <ricky.mandich@gmail.com>
2026-07-16 11:09:00 +0200

    aggiornamento 2026 07 16 11:09 [Beta 0.0.57] - updated vite config to silence sass warning

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,18 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/scss/app.scss', 'resources/js/app.js', 'resources/scss/tombola.scss'],
-            refresh: true,
-        }),
-    ],
+	plugins: [
+		laravel({
+			input: ['resources/css/app.css', 'resources/scss/app.scss', 'resources/js/app.js', 'resources/scss/tombola.scss'],
+			refresh: true,
+		}),
+	],
+	css: {
+		preprocessorOptions: {
+			scss: {
+				quietDeps: true,
+				silenceDeprecations: ['import', 'global-builtin', 'color-functions', 'if-function']
+			}
+		}
+	}
 });
